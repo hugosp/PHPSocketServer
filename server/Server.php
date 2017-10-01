66fd7910e7287f0a2fbb2f5706a1f904b6fad65a
hugosp <stefan.sundell@gmail.com>
2017-10-01 17:38:56 +0200

    fix lazy styling

--- server/Server.php
+++ server/Server.php
@@ -140,8 +140,7 @@ class SocketServer {
 
     private function send_message($msg) {
 
-        foreach($this->clients as $changed_socket)
-        {
+        foreach($this->clients as $changed_socket) {
             @socket_write($changed_socket,$msg,strlen($msg));
         }
         return true;
@@ -154,25 +153,25 @@ class SocketServer {
         if($length == 126) {
             $masks = substr($text, 4, 4);
             $data = substr($text, 8);
-        }
-        elseif($length == 127) {
+        } elseif($length == 127) {
             $masks = substr($text, 10, 4);
             $data = substr($text, 14);
-        }
-        else {
+        } else {
             $masks = substr($text, 2, 4);
             $data = substr($text, 6);
         }
+
         $text = "";
+        
         for ($i = 0; $i < strlen($data); ++$i) {
             $text .= $data[$i] ^ $masks[$i%4];
         }
+        
         return $text;
     }
 
     //Encode message for transfer to client.
-    private function mask($text)
-    {
+    private function mask($text) {
         $b1 = 0x80 | (0x1 & 0x0f);
         $length = strlen($text);
         
@@ -182,25 +181,25 @@ class SocketServer {
             $header = pack('CCn', $b1, 126, $length);
         elseif($length >= 65536)
             $header = pack('CCNN', $b1, 127, $length);
+        
         return $header.$text;
     }
 
     //handshake new client.
-    function perform_handshaking($receved_header,$client_conn, $host, $port)
-    {
+    function perform_handshaking($receved_header,$client_conn, $host, $port) {
         $headers = array();
         $lines = preg_split("/\r\n/", $receved_header);
-        foreach($lines as $line)
-        {
+
+        foreach($lines as $line) {
             $line = chop($line);
-            if(preg_match('/\A(\S+): (.*)\z/', $line, $matches))
-            {
+            if(preg_match('/\A(\S+): (.*)\z/', $line, $matches)) {
                 $headers[$matches[1]] = $matches[2];
             }
         }
 
         $secKey = $headers['Sec-WebSocket-Key'];
         $secAccept = base64_encode(pack('H*', sha1($secKey . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
+
         //hand shaking header
         $upgrade  = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" .
         "Upgrade: websocket\r\n" .
